e700d59aab5045561b8a324d209c0746d1593a44
mrbonur <40200429@esmad.ipp.pt>
2023-02-03 11:56:34 +0000

    pages add

---
+++ ecocampusV2/src/views/index.js
@@ -0,0 +1,12 @@
+export { default as Home } from './Home.vue';
+export { default as Layout } from './Layout.vue';
+export { default as Login } from './Login.vue';
+export { default as Register } from './Register.vue';
+export { default as Activities } from "./Activities.vue";
+export { default as Perfil } from "./Perfil.vue";
+export { default as AdminUsers } from "./AdminUsers.vue";
+export { default as EditProfile } from "./EditProfile.vue";
+export { default as Layout } from "./Layout.vue";
+export { default as List } from "./List.vue";
+export { default as Questions} from "./Questions.vue";
+export { default as Ocorrencias} from "./Ocorrencias.vue";
